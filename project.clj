84f982f9fba6d37e3dacdda60c1dfbd8c5662811
Stephen Sloan <steve@stevesloan.com>
2016-04-01 15:59:58 -0700

    Added profiles to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "https://github.com/polygloton/durable-atom"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :main nil
+  :aot :all
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
